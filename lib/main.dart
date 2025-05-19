d73d313c5a3e1558c1a5c4598bddec040b9d2f49
ArslanAhmad03 <arsl2054016@gmail.com>
2025-05-20 02:19:45 +0500

    This is a Grocery app that I thought of one lazy afternoon.

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      // title: 'Flutter Demo',
 
       theme: FlexThemeData.light(scheme: FlexScheme.greenM3),
 
